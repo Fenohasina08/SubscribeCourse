71725bd8924fd3b91481125847c0b33d2bb1c76c
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-12 09:57:08 +0300

    create class at service,repo,and entity,controller

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users") // Correspond à votre table SQL
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name", length = 255)
+    private String firstName;
+
+    @Column(name = "last_name", length = 255)
+    private String lastName;
+
+    @Column(name = "user_name", length = 50) // Correspond à votre VARCHAR(50)
+    private String userName;
+
+    @Column(unique = true, nullable = false, length = 255)
+    private String email;
+
+    // La relation Many-to-Many
+    @ManyToMany
+    @JoinTable(
+            name = "subscriptions", // Nom de la table de liaison SQL
+            joinColumns = @JoinColumn(name = "user_id"), // Colonne de cette table
+            inverseJoinColumns = @JoinColumn(name = "course_id") // Colonne de la table liée
+    )
+    private Set<Course> courses = new HashSet<>();
+}
